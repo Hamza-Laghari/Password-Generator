f17160ecdd16b6a8d9398bb53802f9ea44ec5e64
Hamza-Laghari <120000289+Hamza-Laghari@users.noreply.github.com>
2023-01-17 08:00:27 +0500

    Update password_generator.js

--- password_generator.js
+++ password_generator.js
@@ -33,28 +33,33 @@ function getRandomCharacter() {
 
 
 function passLength(){
-    alert("Password length set! Ready to generate password!")
+    
+   alert("Ready to generate password!")
 }
 
 function generateRandomPassword() {
     let length = document.querySelector("#passLengthh").value;
     let passwordLength = length
-    if(length<1)
-    {
-        alert("Enter password length")
-    }
+    
+    if(length<16&&length>1){
     let randomPassword = ""
     for (let i = 0; i < passwordLength; i++) {
         randomPassword += getRandomCharacter()         
     }
     pass1.textContent = randomPassword;
     generateRandomPassword2();
-    
+}
+else if(length<1)
+{
+    alert("Enter password length")
+}
+
+else alert("Password length must be less than 15!!")
 }
 
 function generateRandomPassword2() {
     let Length2= document.querySelector("#passLengthh").value;
-    let passwordlength2 = Length2    
+    let passwordlength2 = Length2   
     let randomPassword2 = ""
     if(Length2<1)
     {
@@ -64,6 +69,7 @@ function generateRandomPassword2() {
         randomPassword2 += getRandomCharacter2()        
     }
      pass2.textContent = randomPassword2 
+
 }
 function getRandomCharacter2() {
     const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
